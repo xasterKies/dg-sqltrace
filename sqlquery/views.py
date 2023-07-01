08469039be7d02470fda9d95d355b3e32cc9175f
xasterKies <samueledward40@gmail.com>
2023-07-01 19:28:19 +0100

    chore: populating database with faker and logging it to the client home route

--- sqlquery/views.py
+++ sqlquery/views.py
@@ -3,10 +3,14 @@ from .models import Product
 import json
 from django.http import JsonResponse
 from django.core.serializers import serialize
+from django.db import connection
 
 # Create your views here.
 def home(request):
     qs = Product.objects.all()
     serialized_data = serialize("json", qs)
+
+    print(connection.queries)
+    
     serialized_data = json.loads(serialized_data)
     return JsonResponse(serialized_data, safe=False, status=200)

f0e6b422045c513889cf2fa8a669b291d3c6c0bf
xasterKies <samueledward40@gmail.com>
2023-07-08 14:12:42 +0100

    feat: highlighting an d terminal formater for SQL

--- sqlquery/views.py
+++ sqlquery/views.py
@@ -4,13 +4,20 @@ import json
 from django.http import JsonResponse
 from django.core.serializers import serialize
 from django.db import connection
+from pygments import highlight
+from pygments.formatters import TerminalFormatter
+from pygments.lexers import SqlLexer
+from sqlparse import format
 
 # Create your views here.
 def home(request):
     qs = Product.objects.all()
     serialized_data = serialize("json", qs)
 
-    print(connection.queries)
+    q = list(connection.queries)
+    for qs in q:
+        sqlformatted = format(str(qs["sql"]), reindent=True)
+        print(highlight(sqlformatted, SqlLexer(), TerminalFormatter()))
     
     serialized_data = json.loads(serialized_data)
     return JsonResponse(serialized_data, safe=False, status=200)

160f71ddb0dd0151b8193014aacdd248ffab7a5f
xasterKies <samueledward40@gmail.com>
2023-09-03 19:01:04 +0100

    fix: running from windows

--- dgsqltrace/settings.py
+++ dgsqltrace/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'dgsqltrace.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
